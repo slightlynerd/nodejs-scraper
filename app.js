56b7a4ae86a3126baf593ddc31147f531c5e054d
slightlynerd <gobokel@gmail.com>
2019-02-21 17:09:13 +0100

    removed items that came from second ol tag

--- app.js
+++ app.js
@@ -15,38 +15,38 @@ app.get('/last-fm', (req, res) => {
 
   // url we will scape from
   const URL = 'https://www.last.fm/music/+releases/coming-soon/popular';
-  const titles = [];
-  const coverArts = [];
-  const artists = [];
+  const title = [];
+  const coverArt = [];
+  const artist = [];
   const releaseDates = [];
   const comingSoon = [];
   request(URL, (error, response, html) => {
-    if (error) return;
-
+    if (error) res.json({message: 'An error occured when fetching the page'});
+    
     const $ = cheerio.load(html);
-    $('a.link-block-target').filter(function() {
+    $('ol').first().find('a.link-block-target').filter(function() {
       const data = $(this).text();
-      titles.push(data);
+      title.push(data);
     });
-    $('img.cover-art').filter(function() {
+    $('ol').first().find('img.cover-art').filter(function() {
       const data = $(this).attr('src');
-      coverArts.push(data);
+      coverArt.push(data);
     });
-    $('p.album-grid-item-artist').filter(function() {
+    $('ol').first().find('p.album-grid-item-artist').filter(function() {
       const data = $(this).text();
       const filteredData = data.replace('\n', '').trim();
-      artists.push(filteredData);
+      artist.push(filteredData);
     });
-    $('p.album-grid-item-date').filter(function() {
+    $('ol').first().find('p.album-grid-item-date').filter(function() {
       const data = $(this).text();
       const filteredData = data.replace('\n', '').trim();
       releaseDates.push(filteredData);
     });
-    for (let i = 0; i < titles.length; i++) {
+    for (let i = 0; i < title.length; i++) {
       let obj = {};
-      obj.title = titles[i];
-      obj.coverArt = coverArts[i];
-      obj.artist = artists[i];
+      obj.title = title[i];
+      obj.coverArt = coverArt[i];
+      obj.artist = artist[i];
       obj.releaseDate = releaseDates[i];
       comingSoon.push(obj);
     }
